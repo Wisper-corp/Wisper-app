9d71cd6cc95820f5fef4dd19c28333e62d8fa6ba
Md Aminul Islam <aminulappdev@gmail.com>
2026-07-22 16:24:36 +0600

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 import PushKit
 import flutter_callkit_incoming
 import UserNotifications
+import AVFAudio
+import CallKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, PKPushRegistryDelegate, CallkitIncomingAppDelegate {
@@ -37,5 +39,26 @@ import UserNotifications
   func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
     print("VoIP token invalidated")
     SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP("")
+  }
+
+  func onAccept(_ call: Call, _ action: CXAnswerCallAction) {
+    action.fulfill()
+  }
+
+  func onDecline(_ call: Call, _ action: CXEndCallAction) {
+    action.fulfill()
+  }
+
+  func onEnd(_ call: Call, _ action: CXEndCallAction) {
+    action.fulfill()
+  }
+
+  func onTimeOut(_ call: Call) {
+  }
+
+  func didActivateAudioSession(_ audioSession: AVAudioSession) {
+  }
+
+  func didDeactivateAudioSession(_ audioSession: AVAudioSession) {
   }
 }
