0548c5cbcc00dec9b215856324eb8fea0151dd82
Md Aminul Islam <aminulappdev@gmail.com>
2026-07-22 16:18:48 +0600

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,17 +1,41 @@
 import Flutter
 import UIKit
+import PushKit
+import flutter_callkit_incoming
+import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate, PKPushRegistryDelegate, CallkitIncomingAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     application.registerForRemoteNotifications()
+
+    let mainQueue = DispatchQueue.main
+    let voipRegistry = PKPushRegistry(queue: mainQueue)
+    voipRegistry.delegate = self
+    voipRegistry.desiredPushTypes = [.voIP]
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  func pushRegistry(_ registry: PKPushRegistry, didUpdate credentials: PKPushCredentials, for type: PKPushType) {
+    let deviceToken = credentials.token.map { String(format: "%02x", $0) }.joined()
+    print("VoIP token: \(deviceToken)")
+    SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP(deviceToken)
+  }
+
+  func pushRegistry(_ registry: PKPushRegistry, didInvalidatePushTokenFor type: PKPushType) {
+    print("VoIP token invalidated")
+    SwiftFlutterCallkitIncomingPlugin.sharedInstance?.setDevicePushTokenVoIP("")
   }
 }
