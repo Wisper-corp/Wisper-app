fd2bb60e0d2cb214dd9fd4bce729dca3ae90fed7
Chisomalaoma <admin@xtes.app>
2026-08-02 18:26:52 +0500

    fix: update MainActivity package name to com.wisperapplication.app to fix crash

---
+++ android/app/src/main/kotlin/com/wisperapplication/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wisperapplication.app
+
+import android.content.Intent
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    // Ensure the existing activity handles the new intent (CallKit accept)
+    setIntent(intent)
+  }
+}
